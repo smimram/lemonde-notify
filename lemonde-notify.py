85c363b0207643c4dface15ebd51f2bef71c4e1c
Samuel Mimram <smimram@gmail.com>
2015-11-27 09:12:13 +0100

    Add callback to open browser.

--- lemonde-notify.py
+++ lemonde-notify.py
@@ -5,30 +5,45 @@ import sys
 import time
 import json
 import requests
+import webbrowser
 import gi
 gi.require_version('Notify', '0.7')
-from gi.repository import Notify
+from gi.repository import GLib, Notify
 
 Notify.init("Le Monde")
 
 lastid = 0
 url = 'http://live.lemde.fr/mux.json'
 
-while True:
-    try:
-        feed = requests.get(url).text
-        feed = feed[5:-1]
-        feed = json.loads(feed)
-    except Exception as e:
-        sys.stderr.write("Error: " + str(e) + "\n")
-        feed = []
-    for i in feed:
-        i = i['data']
-        id = i['id']
-        if id > lastid:
-            lastid = id
-            titre = i['titre_court']
-            if not titre.endswith('?'): titre += "."
-            Hello = Notify.Notification.new("Le Monde", titre, "dialog-information")
-            Hello.show()
-    time.sleep(60)
+class App():
+    def __init__(self):
+        self.notification = None
+        self.lastid = 0
+        self.check()
+
+    def notification_cb(self, notification, action_name, link):
+        webbrowser.open(link)
+
+    def check(self):
+        try:
+            feed = requests.get(url).text
+            feed = feed[5:-1]
+            feed = json.loads(feed)
+        except Exception as e:
+            sys.stderr.write("Error: " + str(e) + "\n")
+            feed = []
+        for i in feed:
+            i = i['data']
+            id = i['id']
+            if id > self.lastid:
+                self.lastid = id
+                titre = i['titre_court']
+                link = "http://www.lemonde.fr" + i['link']
+                if not titre.endswith('?'): titre += "."
+                self.notification = Notify.Notification.new("Le Monde", titre, "dialog-information")
+                self.notification.add_action("default", "Voir l'article...", self.notification_cb, link)
+                self.notification.show()
+        GLib.timeout_add_seconds(60, self.check)
+
+app = App()
+GLib.MainLoop().run()
